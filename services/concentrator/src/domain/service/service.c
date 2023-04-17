80f74b87c434da640ebfc5f82e9294138d71500e
solidcris <cssouza@gmail.com>
2023-04-17 08:11:56 -0300

    add write csv function

--- services/concentrator/src/domain/service/service.c
+++ services/concentrator/src/domain/service/service.c
@@ -1,5 +1,8 @@
 #include <service.h>
 #include <string.h>
+#include <stdio.h>
+
+static bool service_create_new_file (service_t *object);
 
 bool service_init (service_t *object)
 {
@@ -7,6 +10,7 @@ bool service_init (service_t *object)
 
     if (object != NULL)
     {
+        memset (object, 0, sizeof (service_t));
         status = repository_init (&object->repository);
     }
 
@@ -32,6 +36,7 @@ bool service_sensor_update_by (service_t *object, sensor_t *sensor)
     if (object != NULL && sensor != NULL)
     {
         status = repository_sensor_update_by (&object->repository, sensor);
+        service_write_csv (object, sensor);
     }
 
     return status;
@@ -61,6 +66,24 @@ bool service_sensor_get_all (service_t *object, sensor_t sensors [SENSOR_AMOUNT]
     return status;
 }
 
+bool service_write_csv (service_t *object, sensor_t *sensor)
+{
+    bool status = false;
+
+    if (object != NULL && sensor != NULL)
+    {
+
+        service_create_new_file (object);
+
+        memset (object->buffer, 0, SERVICE_FILE_BUFFER);
+        snprintf (object->buffer, SERVICE_FILE_BUFFER, SERVICE_FILE_FORMAT, sensor->type, sensor->value, sensor->epoch);
+        sat_file_write (&object->file, object->buffer, strlen (object->buffer));
+        status = true;
+    }
+
+    return status;
+}
+
 bool service_close (service_t *object)
 {
     bool status = false;
@@ -70,5 +93,24 @@ bool service_close (service_t *object)
         status = repository_close (&object->repository);
     }
 
+    return status;
+}
+
+static bool service_create_new_file (service_t *object)
+{
+    bool status = false;
+
+    time_t current = sat_time_get_epoch_now ();
+
+    if (current > (object->last_open + SERVICE_FILE_TIME_OPEN))
+    {
+        snprintf (object->buffer, SERVICE_FILE_BUFFER, "%ld.csv", current);
+
+        sat_file_close (&object->file);
+        sat_file_open (&object->file, object->buffer, sat_file_mode_append);
+        sat_file_write (&object->file, SERVICE_FILE_HEADER, strlen (SERVICE_FILE_HEADER));
+        object->last_open = current;
+        status = true;
+    }
     return status;
 }
--- services/concentrator/src/domain/service/service.h
+++ services/concentrator/src/domain/service/service.h
@@ -4,10 +4,19 @@
 #include <stdbool.h>
 #include <sensor.h>
 #include <repository.h>
+#include <sat.h>
+
+#define SERVICE_FILE_HEADER     "sensor,value, time"
+#define SERVICE_FILE_FORMAT     "\n%d,%.2lf,%ld"
+#define SERVICE_FILE_BUFFER     1024
+#define SERVICE_FILE_TIME_OPEN  5
 
 typedef struct 
 {
     repository_t repository;
+    sat_file_t file;
+    char buffer [SERVICE_FILE_BUFFER];
+    time_t last_open;
 } service_t;
 
 bool service_init (service_t *object);
@@ -15,6 +24,7 @@ bool service_open (service_t *object);
 bool service_sensor_update_by (service_t *object, sensor_t *sensor);
 bool service_sensor_get_by_type (service_t *object, sensor_type_t type, sensor_t *sensor);
 bool service_sensor_get_all (service_t *object, sensor_t sensors [SENSOR_AMOUNT]);
+bool service_write_csv (service_t *object, sensor_t *sensor);
 bool service_close (service_t *object);
 
 #endif/* SERVICE_H */
